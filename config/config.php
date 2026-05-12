03f4867afaba499d572768942dc8b3f13f463738
Gerardo Acosta <gacostaf@gmail.com>
2026-05-12 12:28:15 -0600

    delta

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+  define('DB_ADAPTER', 'mysqli'); 
+  define('DB_HOST', 'localhost'); 
+  define('DB_USER', 'leadtbgs_feng825'); 
+  define('DB_PASS', '60LM4)((20!p9P)S'); 
+  define('DB_NAME', 'leadtbgs_feng825'); 
+  define('DB_PERSIST', true); 
+  define('TABLE_PREFIX', 'foea_'); 
+  define('DB_ENGINE', 'InnoDB'); 
+  define('ROOT_URL', 'https://lead-authority.com/fengoffice'); 
+  define('DEFAULT_LOCALIZATION', 'en_us'); 
+  define('COOKIE_PATH', '/'); 
+  define('DEBUG', false); 
+  define('SEED', 'tw3z0ovulosogrghxhovsuub15vostuu'); 
+  define('DB_CHARSET', 'utf8'); 
+  return true;
+?>
